8ac5a05217f54e2af0b0276b3e94fc2d7b30eab7
dfitch96 <kkarl30@gmail.com>
2024-12-02 18:23:34 -0500

    dynaically resizing the area of the boxes based on number of columns and rows

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
-const INIT_ROWS = 16;
+const INIT_ROWS = 8;
 const INIT_COLS = 16;
+const CONTAINER_SIZE = 960;
 
 
 window.addEventListener('load', () => {
@@ -7,28 +8,43 @@ window.addEventListener('load', () => {
 });
 
 
+function createBox(boxSize){
+    
+    
+    let box = document.createElement("div");
+    box.classList.add("box")
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.addEventListener("mouseenter", () => {
+        box.style.backgroundColor = "grey";
+    });
+
+
+    return box;
+}
+
+
 function initGrid(rows, cols){
 
     let container = document.querySelector(".container");
-
+    let grid = document.querySelector(".grid");
+ 
+    let boxSize = Math.min(Math.floor(CONTAINER_SIZE / rows), Math.floor(CONTAINER_SIZE / cols));
     for(let r = 0; r < rows; r++){
 
         let row = document.createElement("div");
         row.classList.add("row")
+
         for(let c = 0; c < cols; c++){
-            let box = document.createElement("div");
             
-            box.classList.add("box")
-            box.addEventListener("mouseenter", () => {
-                box.style.backgroundColor = "grey";
-            });
-            
-            row.appendChild(box);
+            newBox = createBox(boxSize);
+            row.appendChild(newBox);
         }
 
-        container.appendChild(row);
-
+        grid.appendChild(row);
     }
+
+    container.appendChild(grid);
 
 
 }
