7a0eca09a3b1d0af6b3aacb7a241bf1dfaf0990f
dfitch96 <kkarl30@gmail.com>
2024-12-02 16:45:47 -0500

    added javascript for initializing the grid

---
+++ functions.js
@@ -0,0 +1,29 @@
+const INIT_ROWS = 16;
+const INIT_COLS = 16;
+
+
+window.addEventListener('load', () => {
+    initGrid(INIT_ROWS, INIT_COLS);
+});
+
+
+function initGrid(rows, cols){
+
+    let container = document.querySelector(".container");
+
+    for(let r = 0; r < rows; r++){
+
+        let row = document.createElement("div");
+        row.classList.add("row")
+        for(let c = 0; c < cols; c++){
+            let box = document.createElement("div");
+            box.classList.add("box")
+            row.appendChild(box);
+        }
+
+        container.appendChild(row);
+
+    }
+
+
+}
